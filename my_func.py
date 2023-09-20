e13bbba9d7a7fc981b582830114dc1ba902350a9
qian-chu <qianchu99@gmail.com>
2023-09-20 19:49:32 -0400

    func and test

---
+++ my_func.py
@@ -0,0 +1,11 @@
+def my_plus(x, y):
+    # Buggy code
+    return x + y + 1
+
+def my_minus(x, y):
+    # Buggy code
+    return x - y - 1
+
+def my_times(x, y):
+    # Buggy code
+    return x * y + 1
---
+++ my_test.py
@@ -0,0 +1,7 @@
+from my_func import my_plus, my_minus, my_times
+
+assert my_plus(1, 2) == 1 + 2
+assert my_minus(1, 2) == 1 - 2
+assert my_times(1, 2) == 1 * 2
+
+# We need a my_divide function below
